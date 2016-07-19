72266601ff13247c4705a360f72591ba2bc2dcf4
Larry <larry.fenn@gmail.com>
2016-07-19 00:11:04 -0400

    Revert "New branch - listener - for just listening. updateHues() is the timer callback."
    This reverts commit 87e0d36b0b9adc4750d07b1f6fbc2a11f02d0235.

--- Spectrum/Visualizer.cs
+++ Spectrum/Visualizer.cs
@@ -6,7 +6,11 @@ namespace Spectrum
 {
     public class Visualizer
     {
-        // FFT analysis dicts
+        private List<int> lights;
+        private Random rnd;
+        private String hubaddress = "http://192.168.1.26/api/161d04c425fa45e293386cf241a26bf/";
+
+        // FFT dicts
         private Dictionary<String, double[]> bins;
         private Dictionary<String, float[]> energyHistory;
         private Dictionary<String, float> energyLevels;
@@ -38,15 +42,17 @@ namespace Spectrum
         private bool dropPossible = false;
         private int dropDuration = 0;
         private int target = 0;
-        public bool controlLights;
+        public bool controlLights = true;
+        public bool lightsOff = false;
+        public bool redAlert = false;
         public int brighten = 0;
         public int colorslide = 0;
         public int sat = 0;
         public int needupdate = 0;
-        private float vol = 0;
         
-        public Visualizer(bool controlLights)
+        public Visualizer()
         {
+            rnd = new Random();
             bins = new Dictionary<String, double[]>();
             energyHistory = new Dictionary<String, float[]>();
             energyLevels = new Dictionary<String, float>();
@@ -63,12 +69,18 @@ namespace Spectrum
                 energyLevels.Add(band, 0);
                 energyHistory.Add(band, Enumerable.Repeat((float)0, historyLength).ToArray());
             }
+
+            lights = new List<int>(); // these are the light addresses, as fetched from the hue hub, from left to right
+            lights.Add(2);
+            lights.Add(1);
+            lights.Add(4);
+            lights.Add(5);
+            lights.Add(3);
+            // in the future use the API itself to get the light IDs correctly... also set up the "all lights" group automatically
         }
         
-        // music pattern detection
         public void process(float[] spectrum, float level, float peakChange, float dropQuiet, float dropThreshold, float kickQuiet, float kickChange, float snareQuiet, float snareChange)
         {
-            vol = level;
             processCount++;
             processCount = processCount % historyLength;
             for (int i = 1; i < spectrum.Length/2; i++)
@@ -164,18 +176,133 @@ namespace Spectrum
             silence = (level < .01) && silence;
         }
 
-        // status update for hues
         public void updateHues()
         {
-            // run every tick of the timer
-            if (silence)
+            if (!lightPending)
             {
-                System.Diagnostics.Debug.WriteLine("silence");
+                kickPending = kickPending && totalMax;
+                snarePending = snarePending && totalMax;
+                target = rnd.Next(5);
             }
-            System.Diagnostics.Debug.WriteLine(vol);
+            if (silentMode || !controlLights || lightsOff || redAlert)
+            {
+                if (silentMode || needupdate > 0) // not idling & nonzero lights need to be updated.
+                {
+                    silentModeAlternatingFlag = !silentModeAlternatingFlag;
+                    if (!silentMode || (silentMode && silentModeAlternatingFlag) || !controlLights || lightsOff || redAlert)
+                    {
+                        silentModeHueIndex = (silentModeHueIndex + 10000) % 65535;
+                        silentModeLightIndex = (silentModeLightIndex + 1) % 5;
+                        new System.Net.WebClient().UploadStringAsync(new Uri(hubaddress + laddressHelper(lights[silentModeLightIndex])), "PUT", silent(silentModeHueIndex));
+                        needupdate--;
+                        System.Diagnostics.Debug.WriteLine("Updating.." + needupdate);
+                        System.Diagnostics.Debug.WriteLine("silentMode.." + silentMode);
+                    }
+                }
+            }
+            else if (drop)
+            {
+                if (dropDuration == 0)
+                {
+                    System.Diagnostics.Debug.WriteLine("dropOn");
+                    new System.Net.WebClient().UploadStringAsync(new Uri(hubaddress + "groups/0/action/"), "PUT", dropEffect(true));
+                }
+                // was: dropDuration == 1
+                else if (dropDuration == 4)
+                {
+                    //new System.Net.WebClient().UploadStringAsync(new Uri(hubaddress + "groups/0/action/"), "PUT", dropEffect(false));
+                }
+                else if (dropDuration > 8)
+                {
+                    System.Diagnostics.Debug.WriteLine("dropOff");
+                    drop = false;
+                    dropDuration = -1;
+                }
+                dropDuration++;
+            }
+            else if (kickPending)
+            {
+                if (lightPending)
+                {
+                    new System.Net.WebClient().UploadStringAsync(new Uri(hubaddress + laddressHelper(lights[target])), "PUT", kickEffect(false));
+                    lightPending = false;
+                    kickPending = false;
+                }
+                else
+                {
+                    lightPending = true;
+                    System.Diagnostics.Debug.WriteLine("kickOn");
+                    new System.Net.WebClient().UploadStringAsync(new Uri(hubaddress + laddressHelper(lights[target])), "PUT", kickEffect(true));
+                }
+            }
+            else if (snarePending) // second highest priority: snare hit (?)
+            {
+                if (lightPending)
+                {
+                    new System.Net.WebClient().UploadStringAsync(new Uri(hubaddress + laddressHelper(lights[target])), "PUT", snareEffect(false));
+                    snarePending = false;
+                    lightPending = false;
+                }
+                else
+                {
+                    lightPending = true;
+                    System.Diagnostics.Debug.WriteLine("snareOn");
+                    new System.Net.WebClient().UploadStringAsync(new Uri(hubaddress + laddressHelper(lights[target])), "PUT", snareEffect(true));
+                }
+            }
+            else
+            {
+                idleCounter++;
+                // was: idlecounter > 4
+                if (idleCounter > 2)
+                {
+                    new System.Net.WebClient().UploadStringAsync(new Uri(hubaddress + laddressHelper(lights[target])), "PUT", idle());
+                    idleCounter = 0;
+                }
+            }
+            postUpdate();
         }
 
-        // math helper functions
+        private void postUpdate()
+        {
+            if (controlLights && silence && silentCounter > 40 && !silentMode)
+            {
+                System.Diagnostics.Debug.WriteLine("Silence detected.");
+                silentMode = true;
+            }
+            else if (silence)
+            {
+                silentCounter++;
+            }
+            if (!silence)
+            {
+                silentCounter = 0;
+                silentMode = false;
+            }
+            if (silentModeAlternatingFlag)
+            {
+                if (silentModeSatIndex < 127)
+                {
+                    silentModeSatFall = false;
+                }
+                if (silentModeSatIndex > 380)
+                {
+                    silentModeSatFall = true;
+                }
+                if (silentModeSatFall)
+                {
+                    silentModeSatIndex--;
+                }
+                else
+                {
+                    silentModeSatIndex++;
+                }
+            }
+            // this will be changed in process() UNLESS level < .1 for the duration of process()
+            silence = true;
+            totalMax = false;
+            kickMax = false;
+        }
         private bool windowContains(double[] window, int index)
         {
             return (freqToFFTBin(window[0]) <= index && freqToFFTBin(window[1]) >= index);
@@ -188,6 +315,118 @@ namespace Spectrum
         {
             double[] window = bins[bin];
             return freqToFFTBin(window[1]) - freqToFFTBin(window[0]);
+        }
+        private String laddressHelper(int address)
+        {
+            return "lights/" + address + "/state/";
+        }
+        private String jsonMake(int bri, int hue, int sat, int transitiontime, String alert)
+        {
+            // bri: 1-254 brightness, -1 to do nothing, 0 to turn off
+            // hue: 0-65535 hue, actual color reproduction hardware-dependent
+            // sat: 0-254 saturation, 0 white
+            // transitiontime: ms, -1 to use defaults
+            // alert: none - no effect
+            //        select - a breath cycle (aka flash)
+            //        lselect - breath cycles for 15 seconds
+            String result = "{";
+            if (bri == 0)
+            {
+                result += "\"on\":" + "false" + ",";
+            }
+            else
+            {
+                result += "\"on\":" + "true" + ",";
+            }
+            if (bri != -1)
+            {
+                result += "\"bri\":" + bri + ",";
+            }
+            if (hue != -1)
+            {
+                result += "\"hue\":" + hue + ",";
+            }
+            if (sat != -1)
+            {
+                result += "\"sat\":" + sat + ",";
+            }
+            if (transitiontime != -1)
+            {
+                result += "\"transitiontime\":" + transitiontime + ",";
+            }
+            if (alert != "")
+            {
+                result += "\"alert\":\"" + alert + "\",";
+            }
+            if (bri == 0)
+            {
+                result += "\"effect\":\"colorloop\",";
+            }
+            result = result.TrimEnd(',');
+            result += "}";
+            return result;
+        }
+        private String dropEffect(bool dropOn)
+        {
+            if (dropOn)
+            {
+                int dropColor = rnd.Next(1, 65535);
+                //return "{\"on\": true, \"hue\":" + dropColor + ",\"bri\": 254, \"effect\":\"colorloop\",\"sat\":254,\"transitiontime\":0, \"alert\":\"select\"}";
+                return "{\"alert\":\"select\"}";
+            }
+            else
+            {
+                return "{\"bri\":1,\"effect\":\"colorloop\",\"transitiontime\":2}";
+            }
+        }
+        private String kickEffect(bool on)
+        {
+            if (on)
+            {// used to be hue 300, now -1
+                return jsonMake(254, 300, 254, 1, "none");
+            }
+            else
+            {
+                return jsonMake(1, 300, 254, 2, "none");
+            }
+        }
+        private String snareEffect(bool on)
+        {
+            if (on)
+            {
+                // used to be hue 43000
+                return jsonMake(254, 43000, 254, 1, "none");
+            }
+            else
+            {
+                return jsonMake(1, 43000, 254, 2, "none");
+            }
+        }
+        private String idle()
+        {
+            return jsonMake(0, -1, 254, 20, "none");
+        }
+        private String silent(int index)
+        {
+            if (lightsOff)
+            {
+                return "{\"on\": false}";
+            }
+            if (redAlert)
+            {
+                return "{\"on\": true,\"hue\":1,\"effect\":\"none\",\"sat\":254,\"bri\":1}";
+            }
+            else if (controlLights)
+            {
+                return "{\"on\": true,\"hue\":" + (index + 1) + ",\"effect\":\"none\",\"bri\":1,\"sat\":" + Math.Min(silentModeSatIndex, 254) + ",\"transitiontime\":12}";
+            }
+            else
+            {
+                int newbri = Math.Min(Math.Max(254 + 64 * brighten, 1), 254);
+                int newsat = Math.Min(Math.Max(126 + 63 * sat, 0), 254);
+                int newhue = Math.Min(Math.Max(16384 + colorslide * 4096, 0), 65535);
+                return "{\"on\": true,\"hue\":" + newhue + ",\"sat\":" + newsat + ",\"bri\":" + newbri + ",\"effect\":\"none\"}";
+            }
         }
         private String probe(String band, float current, float avg, float sd, float change)
         {
