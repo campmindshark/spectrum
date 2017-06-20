5e84321c599a0a08e3a22892acb5caa185a391e7
Ashoat <ashoat@gmail.com>
2017-06-20 08:17:08 -0700

    MidiInput distinguishes between deviceIndexes for binding, commandsSinceLastTick, GetKnobValue, and GetNoteVelocity

--- MIDI/MidiInput.cs
+++ MIDI/MidiInput.cs
@@ -12,8 +12,10 @@ using System.ComponentModel;
 namespace Spectrum.MIDI {
 
   using BindingKey = Tuple<MidiCommandType, int>;
+  using InnerBindingKey = Tuple<int, MidiCommandType, int>;
 
   public struct MidiCommand {
+    public int deviceIndex;
     public MidiCommandType type;
     public int index;
     public double value;
@@ -36,16 +38,16 @@ namespace Spectrum.MIDI {
     private Configuration config;
     private Dictionary<int, InputDevice> devices;
     private ConcurrentQueue<MidiCommand> buffer;
-    private Dictionary<int, double> knobValues;
-    private Dictionary<int, double> noteVelocities;
+    private Dictionary<int, Dictionary<int, double>> knobValues;
+    private Dictionary<int, Dictionary<int, double>> noteVelocities;
     private MidiCommand[] commandsSinceLastTick;
-    private Dictionary<BindingKey, List<Binding>> bindings;
+    private Dictionary<InnerBindingKey, List<Binding>> bindings;
 
     public MidiInput(Configuration config) {
       this.config = config;
       this.buffer = new ConcurrentQueue<MidiCommand>();
-      this.knobValues = new Dictionary<int, double>();
-      this.noteVelocities = new Dictionary<int, double>();
+      this.knobValues = new Dictionary<int, Dictionary<int, double>>();
+      this.noteVelocities = new Dictionary<int, Dictionary<int, double>>();
       this.commandsSinceLastTick = new MidiCommand[0];
       this.SetBindings();
       this.config.PropertyChanged += ConfigUpdated;
@@ -58,55 +60,55 @@ namespace Spectrum.MIDI {
     }
 
     private void SetBindings() {
-      this.bindings = new Dictionary<BindingKey, List<Binding>>();
+      this.bindings = new Dictionary<InnerBindingKey, List<Binding>>();
 
-      // Basic state
-      this.AddBinding(MidiCommandType.Note, (index, val) => this.noteVelocities[index] = val);
-      this.AddBinding(MidiCommandType.Knob, (index, val) => this.knobValues[index] = val);
+      foreach (int deviceIndex in this.config.midiDevices.Keys) {
+        if (!this.noteVelocities.ContainsKey(deviceIndex)) {
+          this.noteVelocities[deviceIndex] = new Dictionary<int, double>();
+        }
+        if (!this.knobValues.ContainsKey(deviceIndex)) {
+          this.knobValues[deviceIndex] = new Dictionary<int, double>();
+        }
+        this.AddBinding(
+          new Binding() {
+            key = new BindingKey(MidiCommandType.Note, -1),
+            callback = (index, val) => this.noteVelocities[deviceIndex][index] = val,
+          },
+          deviceIndex
+        );
+        this.AddBinding(
+          new Binding() {
+            key = new BindingKey(MidiCommandType.Knob, -1),
+            callback = (index, val) => this.knobValues[deviceIndex][index] = val,
+          },
+          deviceIndex
+        );
+      }
 
-      // Pull bindings from config
-      var activePresets = this.config.midiDevices.Select((pair) => this.config.midiPresets[pair.Value]);
-      foreach (MidiPreset item in activePresets) {
-        foreach (IMidiBindingConfig config in item.Bindings) {
+      var activePresets = this.config.midiDevices.ToDictionary(
+        (pair) => pair.Key,
+        (pair) => this.config.midiPresets[pair.Value]
+      );
+      foreach (var pair in activePresets) {
+        foreach (IMidiBindingConfig config in pair.Value.Bindings) {
           Binding[] bindings = config.GetBindings(this.config);
           foreach (Binding binding in bindings) {
-            this.AddRawBinding(binding);
+            this.AddBinding(binding, pair.Key);
           }
         }
       }
     }
 
-    private void AddBinding(
-      MidiCommandType commandType,
-      Binding.bindingCallback callback
-    ) {
-      this.AddBinding(commandType, -1, callback);
-    }
-
-    private void AddBinding(
-      MidiCommandType commandType,
-      int index,
-      Action<double> callback
-    ) {
-      this.AddBinding(commandType, index, (i, val) => callback(val));
-    }
-
-    private void AddBinding(
-      MidiCommandType commandType,
-      int index,
-      Binding.bindingCallback callback
-    ) {
-      Binding binding = new Binding();
-      binding.key = new BindingKey(commandType, index);
-      binding.callback = callback;
-      this.AddRawBinding(binding);
-    }
-
-    private void AddRawBinding(Binding binding) {
-      if (!this.bindings.ContainsKey(binding.key)) {
-        this.bindings.Add(binding.key, new List<Binding>());
+    private void AddBinding(Binding binding, int deviceIndex) {
+      var innerBindingKey = new InnerBindingKey(
+        deviceIndex,
+        binding.key.Item1,
+        binding.key.Item2
+      );
+      if (!this.bindings.ContainsKey(innerBindingKey)) {
+        this.bindings.Add(innerBindingKey, new List<Binding>());
       }
-      this.bindings[binding.key].Add(binding);
+      this.bindings[innerBindingKey].Add(binding);
     }
 
     private bool active;
@@ -159,13 +161,15 @@ namespace Spectrum.MIDI {
       this.devices = new Dictionary<int, InputDevice>();
       foreach (var pair in this.config.midiDevices) {
         var device = new InputDevice(pair.Key);
-        device.ChannelMessageReceived += ChannelMessageReceived;
+        device.ChannelMessageReceived +=
+          (sender, e) => ChannelMessageReceived(pair.Key, sender, e);
         device.StartRecording();
         this.devices[pair.Key] = device;
       }
     }
 
     private void ChannelMessageReceived(
+      int deviceIndex,
       object sender,
       ChannelMessageEventArgs e
     ) {
@@ -179,6 +183,7 @@ namespace Spectrum.MIDI {
       if (e.Message.Command == ChannelCommand.Controller) {
         double value = (double)e.Message.Data2 / 127;
         command = new MidiCommand() {
+          deviceIndex = deviceIndex,
           type = MidiCommandType.Knob,
           index = e.Message.Data1,
           value = value,
@@ -189,12 +194,14 @@ namespace Spectrum.MIDI {
       ) {
         double value = (double)e.Message.Data2 / 127;
         command = new MidiCommand() {
+          deviceIndex = deviceIndex,
           type = MidiCommandType.Note,
           index = e.Message.Data1,
           value = value,
         };
       } else if (e.Message.Command == ChannelCommand.ProgramChange) {
         command = new MidiCommand() {
+          deviceIndex = deviceIndex,
           type = MidiCommandType.Program,
           index = e.Message.Data1,
         };
@@ -204,11 +211,11 @@ namespace Spectrum.MIDI {
       this.buffer.Enqueue(command);
 
       List<Binding> triggered = new List<Binding>();
-      BindingKey genericKey = new BindingKey(command.type, -1);
+      var genericKey = new InnerBindingKey(deviceIndex, command.type, -1);
       if (this.bindings.ContainsKey(genericKey)) {
         triggered.AddRange(this.bindings[genericKey]);
       }
-      BindingKey key = new BindingKey(command.type, command.index);
+      var key = new InnerBindingKey(deviceIndex, command.type, command.index);
       if (this.bindings.ContainsKey(key)) {
         triggered.AddRange(this.bindings[key]);
       }
@@ -268,18 +275,24 @@ namespace Spectrum.MIDI {
       return InputDevice.GetDeviceCapabilities(deviceIndex).name;
     }
 
-    public double GetKnobValue(int knob) {
-      if (!this.knobValues.ContainsKey(knob)) {
+    public double GetKnobValue(int deviceIndex, int knob) {
+      if (
+        !this.knobValues.ContainsKey(deviceIndex) ||
+        !this.knobValues[deviceIndex].ContainsKey(knob)
+      ) {
         return -1.0;
       }
-      return this.knobValues[knob];
+      return this.knobValues[deviceIndex][knob];
     }
 
-    public double GetNoteVelocity(int note) {
-      if (!this.noteVelocities.ContainsKey(note)) {
+    public double GetNoteVelocity(int deviceIndex, int note) {
+      if (
+        !this.noteVelocities.ContainsKey(deviceIndex) ||
+        !this.noteVelocities[deviceIndex].ContainsKey(note)
+      ) {
         return 0.0;
       }
-      return this.noteVelocities[note];
+      return this.noteVelocities[deviceIndex][note];
     }
 
     public MidiCommand[] GetCommandsSinceLastTick() {
