2aa67272a77c1621fa843a1a38f8f966a3c391dc
Larry Fenn <larry.fenn@gmail.com>
2022-08-12 09:44:25 -0700

    Remove button

--- Spectrum/Windows/VJHUDWindow.xaml.cs
+++ Spectrum/Windows/VJHUDWindow.xaml.cs
@@ -258,10 +258,8 @@ namespace Spectrum {
       this.Bind("colorPaletteIndex", this.colorPalette7, RadioButton.IsCheckedProperty, BindingMode.TwoWay, new TrueIfValueConverter<int>(6));
       this.Bind("colorPaletteIndex", this.colorPalette8, RadioButton.IsCheckedProperty, BindingMode.TwoWay, new TrueIfValueConverter<int>(7));
       this.Bind("beatInput", this.tempoSelectorHuman, RadioButton.IsCheckedProperty, BindingMode.TwoWay, new TrueIfValueConverter<int>(0));
-      this.Bind("beatInput", this.tempoSelectorMadmom, RadioButton.IsCheckedProperty, BindingMode.TwoWay, new TrueIfValueConverter<int>(1));
       this.Bind("beatInput", this.tempoSelectorLink, RadioButton.IsCheckedProperty, BindingMode.TwoWay, new TrueIfValueConverter<int>(2));
       this.Bind("humanLinkOutput", this.tempoHumanLink, CheckBox.IsCheckedProperty);
-      this.Bind("madmomLinkOutput", this.tempoMadmomLink, CheckBox.IsCheckedProperty);
     }
 
     private void Bind(
