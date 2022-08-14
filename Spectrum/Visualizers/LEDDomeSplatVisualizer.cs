578c4b6e0fbe65fb47b3c3ce5d00fe0a556a884b
budadcabrion <jan-karl@jan-karl.com>
2022-08-14 17:07:15 -0400

    splat visualizer - it works, but is not very good

--- Spectrum/Visualizers/LEDDomeSplatVisualizer.cs
+++ Spectrum/Visualizers/LEDDomeSplatVisualizer.cs
@@ -48,17 +48,33 @@ namespace Spectrum {
 
       double level = this.audio.LevelForChannel(0);
       // Sqrt makes values larger and gives more resolution for lower values
-      //double adjustedLevel = Clamp(Math.Sqrt(level), 0.1, 1);
+      double adjustedLevel = Clamp(Math.Sqrt(level), 0.1, 1);
 
       double progress = this.config.beatBroadcaster.ProgressThroughMeasure;
 
-      buffer.Fade(0.9, 0);
+      buffer.Fade(0.96, 0);
 
       if (progress < this.lastProgress) {
+        var rand = new Random();
+        var cx = Map(rand.NextDouble(), 0, 1, 0.1, 0.9);
+        var cy = Map(rand.NextDouble(), 0, 1, 0.1, 0.9);
+        double radius = adjustedLevel * 0.25;
+        var color = rand.Next() % 8;
+
         for (int i = 0; i < buffer.pixels.Length; i++) {
           var pixel = buffer.pixels[i];
 
-          buffer.pixels[i].color = 0xffffff;
+          var dx = pixel.x - cx;
+          var dy = pixel.y - cy;
+          var dist = Math.Sqrt(dx * dx + dy * dy);
+          if (dist < radius) {
+            buffer.pixels[i].color = this.dome.GetGradientColor(
+              color,
+              dist/radius,
+              0,
+              true
+            );
+          }
         }
       }
 
@@ -72,6 +88,23 @@ namespace Spectrum {
       this.dome.Flush();
     }
 
+    // Clamp value x inside range a-b
+    private static double Clamp(double x, double a, double b) {
+      if (x < a) return a;
+      if (x > b) return b;
+      return x;
+    }
+
+    // Map value x from range a-b to range c-d
+    private static double Map(
+      double x,
+      double a,
+      double b,
+      double c,
+      double d
+    ) {
+      return (x - a) * (d - c) / (b - a) + c;
+    }
   }
 
 }
