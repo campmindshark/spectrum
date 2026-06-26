b393e2ed922cdc61e73c81eade9b740af1ec174e
Larry Fenn <larry.fenn@gmail.com>
2026-06-26 17:56:06 -0400

    Remove Fody Weaver

--- Spectrum/SpectrumConfiguration.cs
+++ Spectrum/SpectrumConfiguration.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Spectrum.Base;
-using PropertyChanged;
 using System.ComponentModel;
 using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
 using System.Xml.Serialization;
 
 namespace Spectrum {
@@ -10,6 +10,15 @@ namespace Spectrum {
   public class SpectrumConfiguration : Configuration {
 
     public event PropertyChangedEventHandler PropertyChanged;
+
+    private void SetField<T>(ref T field, T value,
+        [CallerMemberName] string name = null) {
+      if (EqualityComparer<T>.Default.Equals(field, value)) {
+        return;
+      }
+      field = value;
+      this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 
     public SpectrumConfiguration() {
       this._colorPalette.PropertyChanged += ColorPalettePropertyChanged;
@@ -27,81 +36,297 @@ namespace Spectrum {
       this.PropertyChanged(this, new PropertyChangedEventArgs("midiLog"));
     }
 
-    public string audioDeviceID { get; set; } = null;
+    private string _audioDeviceID = null;
+    public string audioDeviceID {
+      get => _audioDeviceID;
+      set => SetField(ref _audioDeviceID, value);
+    }
 
-    public bool domeEnabled { get; set; } = false;
-    public bool midiInputEnabled { get; set; } = false;
-    public bool barEnabled { get; set; } = false;
-    public bool stageEnabled { get; set; } = false;
+    private bool _domeEnabled = false;
+    public bool domeEnabled {
+      get => _domeEnabled;
+      set => SetField(ref _domeEnabled, value);
+    }
+    private bool _midiInputEnabled = false;
+    public bool midiInputEnabled {
+      get => _midiInputEnabled;
+      set => SetField(ref _midiInputEnabled, value);
+    }
+    private bool _barEnabled = false;
+    public bool barEnabled {
+      get => _barEnabled;
+      set => SetField(ref _barEnabled, value);
+    }
+    private bool _stageEnabled = false;
+    public bool stageEnabled {
+      get => _stageEnabled;
+      set => SetField(ref _stageEnabled, value);
+    }
 
-    public bool midiInputInSeparateThread { get; set; } = false;
-    public bool domeOutputInSeparateThread { get; set; } = false;
-    public bool barOutputInSeparateThread { get; set; } = false;
-    public bool stageOutputInSeparateThread { get; set; } = false;
+    private bool _midiInputInSeparateThread = false;
+    public bool midiInputInSeparateThread {
+      get => _midiInputInSeparateThread;
+      set => SetField(ref _midiInputInSeparateThread, value);
+    }
+    private bool _domeOutputInSeparateThread = false;
+    public bool domeOutputInSeparateThread {
+      get => _domeOutputInSeparateThread;
+      set => SetField(ref _domeOutputInSeparateThread, value);
+    }
+    private bool _barOutputInSeparateThread = false;
+    public bool barOutputInSeparateThread {
+      get => _barOutputInSeparateThread;
+      set => SetField(ref _barOutputInSeparateThread, value);
+    }
+    private bool _stageOutputInSeparateThread = false;
+    public bool stageOutputInSeparateThread {
+      get => _stageOutputInSeparateThread;
+      set => SetField(ref _stageOutputInSeparateThread, value);
+    }
 
     // Whenever adding one of these, also update MainWindow.configPropertiesIgnored
     // to avoid wasted disk I/O whenever these properties update
+    private int _operatorFPS = 0;
     [XmlIgnore]
-    public int operatorFPS { get; set; } = 0;
+    public int operatorFPS {
+      get => _operatorFPS;
+      set => SetField(ref _operatorFPS, value);
+    }
+    private int _domeBeagleboneOPCFPS = 0;
     [XmlIgnore]
-    public int domeBeagleboneOPCFPS { get; set; } = 0;
+    public int domeBeagleboneOPCFPS {
+      get => _domeBeagleboneOPCFPS;
+      set => SetField(ref _domeBeagleboneOPCFPS, value);
+    }
+    private int _barBeagleboneOPCFPS = 0;
     [XmlIgnore]
-    public int barBeagleboneOPCFPS { get; set; } = 0;
+    public int barBeagleboneOPCFPS {
+      get => _barBeagleboneOPCFPS;
+      set => SetField(ref _barBeagleboneOPCFPS, value);
+    }
+    private int _stageBeagleboneOPCFPS = 0;
     [XmlIgnore]
-    public int stageBeagleboneOPCFPS { get; set; } = 0;
+    public int stageBeagleboneOPCFPS {
+      get => _stageBeagleboneOPCFPS;
+      set => SetField(ref _stageBeagleboneOPCFPS, value);
+    }
 
 
-    public string barBeagleboneOPCAddress { get; set; } = "";
-    public bool barSimulationEnabled { get; set; } = false;
-    public int barInfinityWidth { get; set; } = 0;
-    public int barInfinityLength { get; set; } = 0;
-    public int barRunnerLength { get; set; } = 0;
-    public double barBrightness { get; set; } = 0.1;
-    public int barTestPattern { get; set; } = 0;
+    private string _barBeagleboneOPCAddress = "";
+    public string barBeagleboneOPCAddress {
+      get => _barBeagleboneOPCAddress;
+      set => SetField(ref _barBeagleboneOPCAddress, value);
+    }
+    private bool _barSimulationEnabled = false;
+    public bool barSimulationEnabled {
+      get => _barSimulationEnabled;
+      set => SetField(ref _barSimulationEnabled, value);
+    }
+    private int _barInfinityWidth = 0;
+    public int barInfinityWidth {
+      get => _barInfinityWidth;
+      set => SetField(ref _barInfinityWidth, value);
+    }
+    private int _barInfinityLength = 0;
+    public int barInfinityLength {
+      get => _barInfinityLength;
+      set => SetField(ref _barInfinityLength, value);
+    }
+    private int _barRunnerLength = 0;
+    public int barRunnerLength {
+      get => _barRunnerLength;
+      set => SetField(ref _barRunnerLength, value);
+    }
+    private double _barBrightness = 0.1;
+    public double barBrightness {
+      get => _barBrightness;
+      set => SetField(ref _barBrightness, value);
+    }
+    private int _barTestPattern = 0;
+    public int barTestPattern {
+      get => _barTestPattern;
+      set => SetField(ref _barTestPattern, value);
+    }
 
-    public string stageBeagleboneOPCAddress { get; set; } = "";
-    public bool stageSimulationEnabled { get; set; } = false;
-    public int[] stageSideLengths { get; set; } = null;
-    public double stageBrightness { get; set; } = 0.1;
-    public int stageTestPattern { get; set; } = 0;
-    public double stageTracerSpeed { get; set; } = 1.0;
+    private string _stageBeagleboneOPCAddress = "";
+    public string stageBeagleboneOPCAddress {
+      get => _stageBeagleboneOPCAddress;
+      set => SetField(ref _stageBeagleboneOPCAddress, value);
+    }
+    private bool _stageSimulationEnabled = false;
+    public bool stageSimulationEnabled {
+      get => _stageSimulationEnabled;
+      set => SetField(ref _stageSimulationEnabled, value);
+    }
+    private int[] _stageSideLengths = null;
+    public int[] stageSideLengths {
+      get => _stageSideLengths;
+      set => SetField(ref _stageSideLengths, value);
+    }
+    private double _stageBrightness = 0.1;
+    public double stageBrightness {
+      get => _stageBrightness;
+      set => SetField(ref _stageBrightness, value);
+    }
+    private int _stageTestPattern = 0;
+    public int stageTestPattern {
+      get => _stageTestPattern;
+      set => SetField(ref _stageTestPattern, value);
+    }
+    private double _stageTracerSpeed = 1.0;
+    public double stageTracerSpeed {
+      get => _stageTracerSpeed;
+      set => SetField(ref _stageTracerSpeed, value);
+    }
 
-    public string domeBeagleboneOPCAddress { get; set; } = "";
+    private string _domeBeagleboneOPCAddress = "";
+    public string domeBeagleboneOPCAddress {
+      get => _domeBeagleboneOPCAddress;
+      set => SetField(ref _domeBeagleboneOPCAddress, value);
+    }
 
-    public bool domeSimulationEnabled { get; set; } = false;
-    public double domeMaxBrightness { get; set; } = 0.5;
-    public double domeBrightness { get; set; } = 0.1;
-    public int domeVolumeAnimationSize { get; set; } = 4;
-    public int domeAutoFlashDelay { get; set; } = 100;
-    public double domeVolumeRotationSpeed { get; set; } = 1.0;
-    public double domeGradientSpeed { get; set; } = 1.0;
-    public int domeTestPattern { get; set; } = 0;
-    public int domeActiveVis { get; set; } = 0;
-    public double domeGlobalFadeSpeed { get; set; } = 0;
-    public double domeGlobalHueSpeed { get; set; } = 1;
-    public double domeTwinkleDensity { get; set; } = 0;
-    public double domeRippleCDStep { get; set; } = 1;
-    public double domeRippleStep { get; set; } = 1;
-    public int domeRadialEffect { get; set; } = 0;
-    public double domeRadialSize { get; set; } = 0.1;
-    public int domeRadialFrequency { get; set; } = 1;
-    public double domeRadialCenterAngle { get; set; } = 0.0;
-    public double domeRadialCenterDistance { get; set; } = 0.0;
-    public double domeRadialCenterSpeed { get; set; } = 0.0;
+    private bool _domeSimulationEnabled = false;
+    public bool domeSimulationEnabled {
+      get => _domeSimulationEnabled;
+      set => SetField(ref _domeSimulationEnabled, value);
+    }
+    private double _domeMaxBrightness = 0.5;
+    public double domeMaxBrightness {
+      get => _domeMaxBrightness;
+      set => SetField(ref _domeMaxBrightness, value);
+    }
+    private double _domeBrightness = 0.1;
+    public double domeBrightness {
+      get => _domeBrightness;
+      set => SetField(ref _domeBrightness, value);
+    }
+    private int _domeVolumeAnimationSize = 4;
+    public int domeVolumeAnimationSize {
+      get => _domeVolumeAnimationSize;
+      set => SetField(ref _domeVolumeAnimationSize, value);
+    }
+    private int _domeAutoFlashDelay = 100;
+    public int domeAutoFlashDelay {
+      get => _domeAutoFlashDelay;
+      set => SetField(ref _domeAutoFlashDelay, value);
+    }
+    private double _domeVolumeRotationSpeed = 1.0;
+    public double domeVolumeRotationSpeed {
+      get => _domeVolumeRotationSpeed;
+      set => SetField(ref _domeVolumeRotationSpeed, value);
+    }
+    private double _domeGradientSpeed = 1.0;
+    public double domeGradientSpeed {
+      get => _domeGradientSpeed;
+      set => SetField(ref _domeGradientSpeed, value);
+    }
+    private int _domeTestPattern = 0;
+    public int domeTestPattern {
+      get => _domeTestPattern;
+      set => SetField(ref _domeTestPattern, value);
+    }
+    private int _domeActiveVis = 0;
+    public int domeActiveVis {
+      get => _domeActiveVis;
+      set => SetField(ref _domeActiveVis, value);
+    }
+    private double _domeGlobalFadeSpeed = 0;
+    public double domeGlobalFadeSpeed {
+      get => _domeGlobalFadeSpeed;
+      set => SetField(ref _domeGlobalFadeSpeed, value);
+    }
+    private double _domeGlobalHueSpeed = 1;
+    public double domeGlobalHueSpeed {
+      get => _domeGlobalHueSpeed;
+      set => SetField(ref _domeGlobalHueSpeed, value);
+    }
+    private double _domeTwinkleDensity = 0;
+    public double domeTwinkleDensity {
+      get => _domeTwinkleDensity;
+      set => SetField(ref _domeTwinkleDensity, value);
+    }
+    private double _domeRippleCDStep = 1;
+    public double domeRippleCDStep {
+      get => _domeRippleCDStep;
+      set => SetField(ref _domeRippleCDStep, value);
+    }
+    private double _domeRippleStep = 1;
+    public double domeRippleStep {
+      get => _domeRippleStep;
+      set => SetField(ref _domeRippleStep, value);
+    }
+    private int _domeRadialEffect = 0;
+    public int domeRadialEffect {
+      get => _domeRadialEffect;
+      set => SetField(ref _domeRadialEffect, value);
+    }
+    private double _domeRadialSize = 0.1;
+    public double domeRadialSize {
+      get => _domeRadialSize;
+      set => SetField(ref _domeRadialSize, value);
+    }
+    private int _domeRadialFrequency = 1;
+    public int domeRadialFrequency {
+      get => _domeRadialFrequency;
+      set => SetField(ref _domeRadialFrequency, value);
+    }
+    private double _domeRadialCenterAngle = 0.0;
+    public double domeRadialCenterAngle {
+      get => _domeRadialCenterAngle;
+      set => SetField(ref _domeRadialCenterAngle, value);
+    }
+    private double _domeRadialCenterDistance = 0.0;
+    public double domeRadialCenterDistance {
+      get => _domeRadialCenterDistance;
+      set => SetField(ref _domeRadialCenterDistance, value);
+    }
+    private double _domeRadialCenterSpeed = 0.0;
+    public double domeRadialCenterSpeed {
+      get => _domeRadialCenterSpeed;
+      set => SetField(ref _domeRadialCenterSpeed, value);
+    }
 
     // maps from device ID to preset ID
-    public bool vjHUDEnabled { get; set; } = false;
-    public Dictionary<int, int> midiDevices { get; set; } = new Dictionary<int, int>();
-    public Dictionary<int, MidiPreset> midiPresets { get; set; } = new Dictionary<int, MidiPreset>();
+    private bool _vjHUDEnabled = false;
+    public bool vjHUDEnabled {
+      get => _vjHUDEnabled;
+      set => SetField(ref _vjHUDEnabled, value);
+    }
+    private Dictionary<int, int> _midiDevices = new Dictionary<int, int>();
+    public Dictionary<int, int> midiDevices {
+      get => _midiDevices;
+      set => SetField(ref _midiDevices, value);
+    }
+    private Dictionary<int, MidiPreset> _midiPresets = new Dictionary<int, MidiPreset>();
+    public Dictionary<int, MidiPreset> midiPresets {
+      get => _midiPresets;
+      set => SetField(ref _midiPresets, value);
+    }
+    private ObservableMidiLog _midiLog = new ObservableMidiLog();
     [XmlIgnore]
-    public ObservableMidiLog midiLog { get; set; } = new ObservableMidiLog();
+    public ObservableMidiLog midiLog {
+      get => _midiLog;
+      set => SetField(ref _midiLog, value);
+    }
 
-    public Dictionary<string, ILevelDriverPreset> levelDriverPresets { get; set; } = new Dictionary<string, ILevelDriverPreset>();
-    public Dictionary<int, string> channelToAudioLevelDriverPreset { get; set; } = new Dictionary<int, string>();
-    public Dictionary<int, string> channelToMidiLevelDriverPreset { get; set; } = new Dictionary<int, string>();
+    private Dictionary<string, ILevelDriverPreset> _levelDriverPresets = new Dictionary<string, ILevelDriverPreset>();
+    public Dictionary<string, ILevelDriverPreset> levelDriverPresets {
+      get => _levelDriverPresets;
+      set => SetField(ref _levelDriverPresets, value);
+    }
+    private Dictionary<int, string> _channelToAudioLevelDriverPreset = new Dictionary<int, string>();
+    public Dictionary<int, string> channelToAudioLevelDriverPreset {
+      get => _channelToAudioLevelDriverPreset;
+      set => SetField(ref _channelToAudioLevelDriverPreset, value);
+    }
+    private Dictionary<int, string> _channelToMidiLevelDriverPreset = new Dictionary<int, string>();
+    public Dictionary<int, string> channelToMidiLevelDriverPreset {
+      get => _channelToMidiLevelDriverPreset;
+      set => SetField(ref _channelToMidiLevelDriverPreset, value);
+    }
 
     // This probably should not be here...
-    [XmlIgnore, DoNotNotify]
+    [XmlIgnore]
     public BeatBroadcaster beatBroadcaster { get; set; }
 
     private LEDColorPalette _colorPalette = new LEDColorPalette();
@@ -112,31 +337,64 @@ namespace Spectrum {
       set {
         value.PropertyChanged += this.ColorPalettePropertyChanged;
         this._colorPalette = value;
+        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(colorPalette)));
       }
     }
-    public int colorPaletteIndex { get; set; } = 0;
-    public double flashSpeed { get; set; } = 0.0;
+    private int _colorPaletteIndex = 0;
+    public int colorPaletteIndex {
+      get => _colorPaletteIndex;
+      set => SetField(ref _colorPaletteIndex, value);
+    }
+    private double _flashSpeed = 0.0;
+    public double flashSpeed {
+      get => _flashSpeed;
+      set => SetField(ref _flashSpeed, value);
+    }
 
     // Whenever adding one of these, also update MainWindow.configPropertiesIgnored
     // to avoid wasted disk I/O whenever these properties update
-    [XmlIgnore, DoNotNotify]
+    [XmlIgnore]
     public ConcurrentQueue<DomeLEDCommand> domeCommandQueue { get; } =
       new ConcurrentQueue<DomeLEDCommand>();
-    [XmlIgnore, DoNotNotify]
+    [XmlIgnore]
     public ConcurrentQueue<BarLEDCommand> barCommandQueue { get; } =
       new ConcurrentQueue<BarLEDCommand>();
-    [XmlIgnore, DoNotNotify]
+    [XmlIgnore]
     public ConcurrentQueue<StageLEDCommand> stageCommandQueue { get; } =
       new ConcurrentQueue<StageLEDCommand>();
 
     // 0 = human, 1 = Madmom, 2 = Ableton Link
-    public int beatInput { get; set; } = 0;
-    public bool humanLinkOutput { get; set; } = false;
-    public bool madmomLinkOutput { get; set; } = false;
+    private int _beatInput = 0;
+    public int beatInput {
+      get => _beatInput;
+      set => SetField(ref _beatInput, value);
+    }
+    private bool _humanLinkOutput = false;
+    public bool humanLinkOutput {
+      get => _humanLinkOutput;
+      set => SetField(ref _humanLinkOutput, value);
+    }
+    private bool _madmomLinkOutput = false;
+    public bool madmomLinkOutput {
+      get => _madmomLinkOutput;
+      set => SetField(ref _madmomLinkOutput, value);
+    }
 
-    public int orientationDeviceSpotlight { get; set; } = 0;
-    public bool orientationCalibrate { get; set; } = false;
-    public bool orientationShowContours { get; set; } = false;
+    private int _orientationDeviceSpotlight = 0;
+    public int orientationDeviceSpotlight {
+      get => _orientationDeviceSpotlight;
+      set => SetField(ref _orientationDeviceSpotlight, value);
+    }
+    private bool _orientationCalibrate = false;
+    public bool orientationCalibrate {
+      get => _orientationCalibrate;
+      set => SetField(ref _orientationCalibrate, value);
+    }
+    private bool _orientationShowContours = false;
+    public bool orientationShowContours {
+      get => _orientationShowContours;
+      set => SetField(ref _orientationShowContours, value);
+    }
   }
 
 }
